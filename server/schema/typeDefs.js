ecda34f260cf67ad017a217bfcf699f12378b254
Caeldeth <sabrael@hybrasyl.com>
2022-01-22 17:19:11 -0600

    add queries, mutations, user/book/auth types, and savebook input

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,7 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
 
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input saveBookInput {
+        authors: [String]!
+        description: String!
+        title: String!
+        bookId: String!
+        image: String!
+        link: String!
+    }
+
+    type Query {
+        me: User
+    }
+
+    tpe Mutation {
+        login:(email: String!, password: String!): Auth
+        addUser:(email: String!, username: String!, password: String!): Auth
+        saveBook:(input: saveBookInput): User
+        removeBook:(bookId: String!): User
+    }
 `
 
 module.exports = typeDefs;
+
